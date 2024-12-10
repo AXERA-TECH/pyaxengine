721ad0040d3f9978956a6ba130298cb5cf8914d7
zylo117 <zylo117@hotmail.com>
2024-12-10 12:37:31 +0800

    自动判断平台

--- axengine/session.py
+++ axengine/session.py
@@ -1,26 +1,24 @@
 import ctypes.util
 import os
 
+from ._session import BaseInferenceSession
 from .ax_session import InferenceSession as AXInferenceSession
 from .axcl_session import InferenceSession as AXCLInferenceSession
 
 
-class InferenceSession(AXCLInferenceSession, AXInferenceSession):
-    def __init__(self,
-                 path_or_bytes: str | bytes | os.PathLike,
-                 device_id: int = -1):
-        is_axcl = False
-        if device_id >= 0:
-            if ctypes.util.find_library('axcl_rt') is not None:
-                is_axcl = True
-            elif ctypes.util.find_library('ax_engine') is not None:
-                is_axcl = False
-            else:
-                print("axcl_rt not found, please install axcl_host driver")
-
-        if is_axcl:
-            print(f"Using axclrt backend, device_no: {device_id}")
-            AXCLInferenceSession.__init__(self, path_or_bytes, device_id)
+def InferenceSession(path_or_bytes: str | bytes | os.PathLike, device_id: int = -1, *args, **kwargs):
+    is_axcl = False
+    if device_id >= 0:
+        if ctypes.util.find_library('axcl_rt') is not None:
+            is_axcl = True
+        elif ctypes.util.find_library('ax_engine') is not None:
+            is_axcl = False
         else:
-            print("Using ax backend with onboard npu")
-            AXInferenceSession.__init__(self, path_or_bytes)
+            print("axcl_rt not found, please install axcl_host driver")
+
+    if is_axcl:
+        print(f"Using axclrt backend, device_no: {device_id}")
+        return AXCLInferenceSession(path_or_bytes, device_id)
+    else:
+        print("Using ax backend with onboard npu")
+        return AXInferenceSession(path_or_bytes)
